a5b41c546e1d5df6baa31411df9fdc6fb75585a3
Natanael Pacheco <natepacheco716@gmail.com>
2023-08-15 22:11:10 -0500

    Installed all needed dependencies.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      theme: {
+        container: {
+          center: true,
+          padding: "1rem",
+        },
+        colors: {
+          current: "currentColor",
+          transparent: "transparent",
+          "body-color": "#DCDCDC",
+          white: "#FFFFFF",
+          black: "#000000",
+          night: "#020130",
+          sunny: "#FFE14A",
+          morning: "#FF7C53",
+          rain: "#FF64D4",
+          snow: "#42C6FF",
+        },
+        screens: {
+          sm: "480px", // 480px and below (phone landscape & smaller)
+          md: "768px", // 768px and below (tablet)
+          lg: "1024px", // 1024px and below (tablet landscape)
+          xl: "1280px", // 1280px and below (desktop)
+          "2xl": "1441px", // 1441px and below (xlarge screens)
+          "3xl": "1921px", // 1921px and below (xxlarge screens)
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
